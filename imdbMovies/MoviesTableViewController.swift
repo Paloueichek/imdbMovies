b63cadaf1121c55a0f29a14890c213666673e4fe
Patrick Aloueichek <patryk.aloueichek@netguru.pl>
2019-02-13 09:01:04 +0100

    Add proper Xib naming

--- imdbMovies/MoviesTableViewController.swift
+++ imdbMovies/MoviesTableViewController.swift
@@ -12,27 +12,35 @@ class MoviesTableViewController:  UIViewController, UITableViewDelegate, UITable
     
     @IBOutlet weak var moviesTableView: UITableView!
     
-    let items = ["Item 1", "Item2", "Item3", "Item4"]
+   private var movies = [Movies]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        moviesTableView.register(UINib(nibName: "MoviesTableViewController", bundle: nil), forCellReuseIdentifier: "moviesCell")
+        moviesTableView.register(UINib(nibName: MoviesTableViewCell.className, bundle: nil), forCellReuseIdentifier: "moviesCell")
         moviesTableView.delegate = self
         moviesTableView.dataSource = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     return  items.count
+     return  movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        //let cell = Bundle.main.loadNibNamed("MoviesTableViewCell", owner: self, options: nil)?.first as! MoviesTableViewCell
   
         let cell = moviesTableView.dequeueReusableCell(withIdentifier: "moviesCell") as! MoviesTableViewCell
-
         
         return cell
     }
-  
+}
+
+extension NSObject {
+    var className: String {
+        return String(describing: type(of: self))
+    }
+    
+    class var className: String {
+        return String(describing: self)
+    }
 }
