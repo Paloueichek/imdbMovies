95faaf117341fed6b02837960ce3d994bbf8f7ce
Patrick Aloueichek <p.aloueichek@gmail.com>
2019-02-14 12:42:34 +0100

    Add KingFisher and pods

---
+++ Pods/Target Support Files/Pods-imdbMovies/Pods-imdbMovies-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_imdbMovies : NSObject
+@end
+@implementation PodsDummy_Pods_imdbMovies
+@end
---
+++ Pods/Target Support Files/Pods-imdbMoviesTests/Pods-imdbMoviesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_imdbMoviesTests : NSObject
+@end
+@implementation PodsDummy_Pods_imdbMoviesTests
+@end
